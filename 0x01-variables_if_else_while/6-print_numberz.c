5674ef65e68f3e22e3dd91e2bef0d0df69522652
chihab37 <chihab37@gmail.com>
2023-03-17 07:19:35 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - prints the lowercase alphabet in reverse
+  *Return: 0 success
+  */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
