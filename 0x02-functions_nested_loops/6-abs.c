8cfe6edc27df19326ff292372213e0919d67fdb4
chihab37 <chihab37@gmail.com>
2023-03-21 15:11:45 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs - computes the absolute value of an int*
+  *Return: 0
+  *@n: number
+  */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
