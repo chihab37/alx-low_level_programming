35a7831c5920e24b38864ec910dcf62a80583c21
chihab37 <chihab37@gmail.com>
2023-03-21 17:12:34 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+  *times_table - prints 9 table strating from 0
+  */
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			n = j * i;
+			if (j == 0)
+			{
+				_putchar(n + '0');
+			}
+			if (n < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+			else if (n >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
