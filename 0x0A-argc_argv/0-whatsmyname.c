9a1674c902b9238ba323444545ecd9980e3460b7
chihab37 <chihab37@gmail.com>
2023-04-07 03:52:40 -0700

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
-  *whats_myname - prints it name
+  *main - prints it name
   *@argc: number of commands
   *@argv: array of strings contains commands
+  *Retun: 0 success
   */
-void whats_myname(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	    printf("%s\n", argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
