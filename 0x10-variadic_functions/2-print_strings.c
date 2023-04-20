7cb247c0ada71c0f80fc362f9266884b01fb7258
chihab37 <chihab37@gmail.com>
2023-04-20 09:44:56 -0700

    0x01&

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <string.h>
+/**
+  *print_strings - a function that prints strings
+  *@separator: separates strings printed
+  *@n: number of string passed to the func
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *str;
+	va_list args;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char*);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
